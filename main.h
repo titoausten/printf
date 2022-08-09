7a63e42369323dc0089b1e078faab3a6fcbc78ba
prechilala <puzoma16@yahoo.com>
2022-08-09 16:19:27 -0700

    hot fix

--- main.h
+++ main.h
@@ -1,7 +1,61 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <limits.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int _putchar(char c);
+/* Flag Modifier Macros */
+#define PLUS 1
+#define SPACE 2
+#define HASH 4
+#define ZERO 8
+#define NEG 16
+#define PLUS_FLAG (flags & 1)
+#define SPACE_FLAG ((flags >> 1) & 1)
+#define HASH_FLAG ((flags >> 2) & 1)
+#define ZERO_FLAG ((flags >> 3) & 1)
+#define NEG_FLAG ((flags >> 4) & 1)
+
+/* Length Modifier Macros */
+#define SHORT 1
+#define LONG 2
+
+/**
+ * struct bufer_s - A new type defining a bufer struct.
+ * @bufer: A pointer to a character array.
+ * @start: A pointer to the start of bufer.
+ * @len: The length of the string stored in bufer.
+*/
+typedef struct bufer_s
+{
+	char *bufer;
+	char *start;
+	unsigned int len;
+} bufer_t;
+
+/**
+ * struct converter_s - A new type defining a converter struct.
+ * @specifier: A character representing a conversion specifier.
+ * @f: A pointer to a conversion function corresponding to specifier.
+*/
+typedef struct converter_s
+{
+	unsigned char specifier;
+	unsigned int (*f)(va_list, bufer_t *, unsigned char, int, int, unsigned char);
+} converter_t;
+
+/**
+ * struct flag_s - A new type defining a flags struct.
+ * @flag: A character representing a flag.
+ * @value: The integer value of the flag.
+*/
+typedef struct flag_s
+{
+	unsigned char flag;
+	unsigned char value;
+} flag_t;
+
 int _printf(const char *format, ...);
 
 #endif
